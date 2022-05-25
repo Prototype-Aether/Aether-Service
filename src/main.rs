c7b5284a7afabee421e57cc52e86fa7e5a47d13f
Anish Sharma <aneeshsharma15@outlook.com>
2022-05-25 13:02:55 +0530

    error handling for closed connection

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,84 @@
 mod socket_server;
 
+use std::io::ErrorKind;
 use std::io::Read;
 use std::io::Write;
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::SocketAddr;
 use std::thread;
 use std::usize;
 
+use aether_lib::peer::Aether;
+use log::error;
+use log::info;
+use log::trace;
+use simple_logger::SimpleLogger;
 use socket_server::socket_server::init_socket_server;
 
 pub const RECV_BUFFER_SIZE: usize = 2048;
 
-fn parse_data(data: &mut String, buf: &[u8]) -> usize {
-    let mut n: String = String::new();
-    let mut nn: usize = 0;
-
-    println!("{}", String::from_utf8_lossy(buf));
-
-    for i in 0..buf.len() {
-        if buf[i] >= 48 && buf[i] <= 57 {
-            n.push(buf[i] as char);
-        } else {
-            nn = n.parse::<usize>().unwrap();
-            break;
-        }
-    }
-    let data_ = String::from_utf8_lossy(&buf[1..(nn + 1)]).to_string();
-    data.push_str(&data_);
-    return nn;
-}
-
-fn parse_command(buf: &mut [u8; RECV_BUFFER_SIZE]) {
-    let n = buf.len();
-    match buf[0] {
-        // 1 for connect
-        49 => {
-            println!("{}", "connect");
-            let mut username = String::new();
-            let n = parse_data(&mut username, &buf[1..n]);
-            println!("{}", username);
-
-            // Put aether connect here
-        }
-        // 2 for send
-        50 => {
-            println!("{}", "send");
-            let mut username = String::new();
-            let n1 = parse_data(&mut username, &buf[1..n]);
-            println!("{}", username);
-            let mut message = String::new();
-            let n2 = parse_data(&mut message, &buf[n1 + 2..n]);
-            println!("{}", message);
-
-            // Put aether send here
-        }
-        // 3 for recv
-        51 => {
-            println!("{}", "recv");
-            let mut username = String::new();
-            let n = parse_data(&mut username, &buf[1..n]);
-
-            // Put aether recv here
-        }
-        _ => println!("{}", "unknown"),
-    }
-}
-
 fn handle_client(mut client: (impl Read + Write)) {
+    info!("Client connected!");
     loop {
         let mut buf = [0; RECV_BUFFER_SIZE];
-        let n: usize = client.read(&mut buf).unwrap();
+        let n: usize = match client.read(&mut buf) {
+            Ok(n) => n,
+            Err(error) => match error.kind() {
+                ErrorKind::ConnectionReset => {
+                    println!("{:?}", error);
+                    info!("Connection Closed");
+                    break;
+                }
+                _ => {
+                    error!("Unexpected error handling client: {}", error);
+                    break;
+                }
+            },
+        };
+
         if n > 0 {
-            println!("accepting command");
-            parse_command(&mut buf);
+            trace!("Received command");
+
+            // handle command
+
+            // respond to client
+            match client.write(&buf) {
+                Ok(_) => (),
+                Err(error) => match error.kind() {
+                    ErrorKind::ConnectionReset => {
+                        info!("Connection Closed");
+                        break;
+                    }
+                    _ => {
+                        error!("Unexpected error handling client: {}", error);
+                    }
+                },
+            }
         }
     }
 }
 
 fn main() -> std::io::Result<()> {
-    println!("Aether Service started ... ");
+    SimpleLogger::new()
+        .env()
+        .with_level(log::LevelFilter::Info)
+        .init()
+        .expect("Unable to initialize logger");
+
+    info!("Aether Service started...");
+
+    let tracker_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(149, 129, 129, 226)), 8982);
+    let aether = Aether::new(tracker_addr);
+
+    aether.start();
+
     let listener = init_socket_server().unwrap();
 
     // accept connections and process them, spawning a new thread for each one
     for client in listener {
         thread::spawn(|| handle_client(client));
     }
+
     Ok(())
 }
